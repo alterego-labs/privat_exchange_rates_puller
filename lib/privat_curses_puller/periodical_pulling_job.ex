0bb8e77202ec8f85b30bdcc43378ec7402ae03ed
Sergey Gernyak <sergeg1990@gmail.com>
2016-12-29 10:36:31 +0200

    Basic implementation of the pulling job

--- lib/privat_curses_puller/periodical_pulling_job.ex
+++ lib/privat_curses_puller/periodical_pulling_job.ex
@@ -1,7 +1,24 @@
 defmodule PrivatCursesPuller.PeriodicalPullingJob do
   require Logger
 
+  alias PrivatCursesPuller.PrivatAPI
+  alias PrivatCursesPuller.NotificationsAPI
+  alias PrivatCursesPuller.Core.RatesProcessor
+
   def call do
     Logger.info "PeriodicalPullingJob called..."
+    cashless_rates = PrivatAPI.get_cashless_rates
+    auth_info = PrivatAPI.auth_commercial_api(client_id, client_secret)
+    commercial_rates = PrivatAPI.get_commercial_rates(auth_info.id)
+    notification_info = RatesProcessor.call(commercial_rates, cashless_rates)
+    NotificationsAPI.notify :slack, notification_info
+  end
+
+  defp client_id do
+    Application.get_env :privat_curses_puller, :privat_client_id
+  end
+
+  defp client_secret do
+    Application.get_env :privat_curses_puller, :privat_client_secret
   end
 end
